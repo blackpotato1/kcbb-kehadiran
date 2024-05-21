b2321968d701f2ee2c82352e8d81e1885148248b
Lee Hoon Lim <hoonlim123@gmail.com>
2024-05-21 16:18:23 +0800

    your stuff

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="header.css" />
+</head>
+<body>
+    <div id="title"> <p id="name"> Kehadiran Cemerlang Bintang Bersinar </p></div>
+    <div id="links"> 
+        <a>Analisis Kehadiran</a>
+        <a><input type="dropdown"></a>
+        <a>Logout</a> 
+        <a> Liew Xian Yang</a></div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <?php
+        require_once 'header.php' ;
+        ?>
+    <form id="login-box" action="analisis_kehadiran.php">
+        <h1 id="login-header">LOGIN</h1>
+        <p>Nama Pengguna</p>
+        <input>
+        <p>Katalaluan</p>
+        <input type="password" id="password" name="password" required>
+        <a href="#">Lupa Nama Pengguna atau Katalaluan anda?</a>
+        <button type="submit" >LOGIN</button>
+        <p id="asd">Tidak ada akaun?</p>
+        <a href="#">Daftar sekarang hanya mengambil masa 2 minit</a>
+    </form>
+</body>
+</html>
