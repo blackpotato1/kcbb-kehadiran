6c607595546edce5c0684cefd4797414f9088a67
Lee Hoon Lim <hoonlim123@gmail.com>
2024-05-21 17:21:19 +0800

    test

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
 <body>
+    hello world
     <?php
-        require_once 'header.php' ;
-        ?>
+    require_once 'header.php';
+    ?>
     <form id="login-box" action="analisis_kehadiran.php">
         <h1 id="login-header">LOGIN</h1>
         <p>Nama Pengguna</p>
@@ -17,9 +20,10 @@
         <p>Katalaluan</p>
         <input type="password" id="password" name="password" required>
         <a href="#">Lupa Nama Pengguna atau Katalaluan anda?</a>
-        <button type="submit" >LOGIN</button>
+        <button type="submit">LOGIN</button>
         <p id="asd">Tidak ada akaun?</p>
         <a href="#">Daftar sekarang hanya mengambil masa 2 minit</a>
     </form>
 </body>
+
 </html>
